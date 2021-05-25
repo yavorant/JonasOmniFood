41a3a18c1076f4a8698cb0b1692aa41aebddf007
yavorant <yavorant>
2021-05-25 15:48:10 +0300

    Add smooth scrolling from css-tricks.com

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,57 +1,7 @@
 
 
-    /*
-    $('.js--section-features').waypoint(function(direction) {
-          if (direction == "down") {
-              $('nav').addClass('sticky');
-          } else {
-            $('nav').removeClass('sticky');
-          }
-        }
-      }) */
+    /* For the sticky nav apearing */
 
-      
-        
-
-
-/* waypoints example:
-
-var waypoint = new Waypoint({
-  element: document.getElementById('direction-waypoint'),
-  handler: function(direction) {
-    notify('Direction: ' + direction)
-  }
-})
-
-document.querySelector('.js--section-features');
-
-var waypoint = new Waypoint({
-    element: document.getElementById('px-offset-waypoint'),
-    handler: function(direction) {
-      notify('I am 20px from the top of the window')
-    },
-    offset: 20 
-  })
-
-    var waypoint = new Waypoint({
-        element: document.querySelector('.js--section-features'),
-        handler: function(direction) {
-        notify('I am 20px from the top of the window')
-        },
-        offset: 20 
-    })
-*/
-
-
-
-/*
-$(window).scroll(function() {
-    if ($(this).scrollTop() > -200) {
-        $('nav').addClass("sticky");
-    } else {
-        $('nav').removeClass("sticky");
-    }
-*/
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 
@@ -61,3 +11,55 @@ $(window).scroll(function() {
         $("nav").removeClass("sticky");
     }
 });    
+
+    /* Activate buttons: */
+
+// go to plan section
+$('.js--scroll-to-plans').click(function() {
+    $("html, body").animate({ scrollTop: $('.js--section-plans').offset().top}, 1000);
+});
+
+// go to feature section
+
+$('.js--scroll-to-start').click(function() {
+    $("html, body").animate({ scrollTop: $('.js--section-features').offset().top}, 1000);
+});
+
+    /* Smooth Scroll from https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
+
+    // Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+});
